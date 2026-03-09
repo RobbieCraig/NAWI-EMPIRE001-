9dbef17a3b661967606636b96104b32f8137fc3e
NAWI-EMPIRE <akpanvictor848@gmail.com>
2026-03-09 02:10:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// Tell the server to look for files in your main folder
+app.use(express.static(path.join(__dirname)));
 
+// Route for the Home Page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route for all other pages (Live, Market, Games, etc.)
+app.get('/:page', (req, res) => {
+  res.sendFile(path.join(__dirname, req.params.page));
 });
 
 app.listen(PORT, () => {
-    console.log('NAWI-EMPIRE Engine Active');
+  console.log(`NAWI EMPIRE Engine Active on Port ${PORT}`);
 });
